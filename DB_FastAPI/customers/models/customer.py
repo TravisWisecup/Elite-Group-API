d5f3328e204c81e233d737fff3948dfde8288e74
TravisWisecup <traviswisecup@gmail.com>
2023-01-25 16:31:52 +0000

    Updating models to follow new solution

--- DB_FastAPI/customers/models/customer.py
+++ DB_FastAPI/customers/models/customer.py
@@ -1,15 +1,9 @@
 from pydantic import BaseModel
 from customers.models.address import Address
-from customers.models.account import Account
 
 class Customer(BaseModel):
     id: int
     first_name: str
     last_name: str
-    address_id: int
+    address_id: Address
     email: str
-
-    def __eq__(self, other):
-        return self.id == other.id and self.first_name == other.first_name and \
-            self.last_name == other.last_name and self.address_id == other.address_id and \
-            self.email == other.email
