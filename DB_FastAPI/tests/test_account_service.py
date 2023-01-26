9aa6cfc348a47b6707502daa6dcfb3b1da7cbfe2
cytest1111 <christopher.y1111@gmail.com>
2023-01-26 18:38:05 +0000

    small update

--- DB_FastAPI/tests/test_account_service.py
+++ DB_FastAPI/tests/test_account_service.py
@@ -11,30 +11,27 @@ from customers.repositories.customer import Customer
 
 class TestOrderService(unittest.TestCase):
     def setUp(self):
-        self.accountRepository = Account();
-        self.addressRepository = Address();
-        self.customerRepository = Customer();
+        self.accountRepository = Account()
+        self.addressRepository = Address()
+        self.customerRepository = Customer()
 
-        self.customer = Customer(id=1, firstname="John", lastname='Doe'
-                          description="desc", unit_cost=1.00)
+        self.customer = Customer (id=1, firstname="John", lastname='Doe', address_id =1, email = "garbageman@gmail.com")
         self.address = Address(id=1, address="pizza street", city="chilltown", state="CA", zip_code=12345)
         self.account = Account(id=1, account_num =123,
                           customer_id= 1, current_balance= 50.00)
-    
-
-        
 
         #self. = (id=1, order_number="456",
          #                  product=self.product, quantity=1, total=1.00)
-        self.product_repository = Mock()
-        self.orderRepository = Mock()
-        self.customerRepository = Customer(self.orderRepository, self.product_repository)
+        self.customer_repo = Mock()
+        self.address_repo = Mock()
+        self.account_repo = Account(self.account, self.customer, self.address)
 
     def test_add_new(self):
-        self.product_repository.get_by_id = Mock(return_value=self.product)
-        self.orderRepository.insert = Mock(return_value=self.order)
-        new_order = self.orderService.add_new(self.order)
-        self.assertEqual(new_order, self.order)
+        self.account.get_by_num = Mock(return_value=self.account)
+        self.address.insert = Mock(return_value=self.address)
+        self.customer.insert = Mock (return_value= self.customer)
+        new_account = self.account.add_new(self.account)
+        self.assertEqual(new_account, self.order)
 
     def test_get_by_num(self):
         self.orderRepository.get_by_number = Mock(return_value=self.order)
